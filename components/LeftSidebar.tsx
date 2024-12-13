5395d175bf1dcc9d1b89b7b6ceb97c1e700ad653
Aniket98Misra <misra98aniket@gmail.com>
2024-12-13 23:53:02 +0530

    convex setup

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,48 +1,16 @@
-// import Link from "next/link";
-// import React from "react";
-// import Image from "next/image";
-// import { sidebarLinks } from "@/constants";
-// const LeftSidebar = () => {
-//   return (
-//     <section className="left_sidebar">
-//       <nav className="flex flex-col gap-6 ">
-//         <Link
-//           href="/"
-//           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
-//         >
-//           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-//           <h1 className="text-24 font-extrabold text-white max-lg:hidden">
-//             SmartPodify
-//           </h1>
-//         </Link>
-//         {sidebarLinks.map((route, label, imgURL) => {
-//           return (
-//             <Link
-//               href={route}
-//               key={label}
-//               className="flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start "
-//             >
-//               <Image src={imgURL} alt={label} width={24} height={24} />
-//               <p>{label}</p>
-//             </Link>
-//           );
-//         })}
-//       </nav>
-//     </section>
-//   );
-// };
-
-// export default LeftSidebar;
+"use client";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 import { sidebarLinks } from "@/constants";
-
+import { usePathname, useRouter } from "next/navigation";
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6 ">
-        {/* Logo Section */}
         <Link
           href="/"
           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
@@ -52,21 +20,74 @@ const LeftSidebar = () => {
             SmartPodify
           </h1>
         </Link>
-
-        {/* Sidebar Links */}
-        {sidebarLinks.map(({ route, label, imgURL }, index) => (
-          <Link
-            href={route}
-            key={index}
-            className="flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start"
-          >
-            <Image src={imgURL} alt={label} width={24} height={24} />
-            <p className="text-white max-lg:hidden">{label}</p>
-          </Link>
-        ))}
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
 };
 
 export default LeftSidebar;
+
+// "use client";
+// import Link from "next/link";
+// import React from "react";
+// import Image from "next/image";
+// import { cn } from "@/lib/utils";
+// import { sidebarLinks } from "@/constants";
+// import { usePathname, useRouter } from "next/navigation";
+
+// const LeftSidebar = () => {
+//   const pathname = usePathname();
+//   const router = useRouter();
+//   return (
+//     <section className="left_sidebar">
+//       <nav className="flex flex-col gap-6 ">
+//         {/* Logo Section */}
+//         <Link
+//           href="/"
+//           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+//         >
+//           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+//           <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+//             SmartPodify
+//           </h1>
+//         </Link>
+
+//         {/* Sidebar Links */}
+//         {sidebarLinks.map(({ route, label, imgURL }, index) => {
+//           const isActive =
+//             pathname === route || pathname.startsWith(`${route}/`);
+//           <Link
+//             href={route}
+//             key={index}
+//             className={cn(
+//               "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+//               { "bg-nav-focus border-r-4 border-orange-1": isActive }
+//             )}
+//           >
+//             <Image src={imgURL} alt={label} width={24} height={24} />
+//             <p className="text-white max-lg:hidden">{label}</p>
+//           </Link>;
+//         })}
+//       </nav>
+//     </section>
+//   );
+// };
+
+// export default LeftSidebar;
